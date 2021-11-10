edb256c35a6063c27d35f552f060ec97cc383dcb
alinashev <al.t.shevchenko@gmail.com>
2021-11-10 21:30:00 +0200

    Removed Indian code

--- Executor.py
+++ Executor.py
@@ -7,16 +7,17 @@ class Executor():
     def run_executor(json_string, executor_class):
         executor = executor_class()
         start_time_thread = time.time()
-        fututres_1 = executor.submit(Counter.counting,
-                                     json_string=json_string,
-                                     start_value=0,
-                                     end_value=int(len(json_string['questions']) / 2))
-        fututres_2 = executor.submit(Counter.counting,
-                                     json_string=json_string,
-                                     start_value=int(len(json_string['questions']) / 2),
-                                     end_value=len(json_string['questions']))
+        half = int(len(json_string['questions']) / 2)
+
+        result = 0
+        inter = [0, int(len(json_string['questions']) / 2), len(json_string['questions'])]
+        for i in range(0, len(inter) - 1):
+            futures = executor.submit(Counter.counting,
+                                      json_string=json_string,
+                                      start_value=inter[i],
+                                      end_value=inter[i+1])
+            result += futures.result()
         spent_time = time.time() - start_time_thread
-        result = fututres_2.result() + fututres_1.result()
         print("Results:{result}. Executor:{executor}. Time:{spent_time}".format(
             result=result,
             executor=executor_class.__name__,
